09b9e430b68721e7b7cb1e81fdc9a5e23fe0b2be
firdausraginda <ragindafirdaus01@gmail.com>
2022-01-05 20:30:54 +0700

    rename video_put_args to video_post_args

--- main2.py
+++ main2.py
@@ -27,12 +27,12 @@ class VideoModel(db.Model):
 
 # create request parser object
 # 'request parser object' will validate the args that received
-video_put_args = reqparse.RequestParser()
-video_put_args.add_argument(
+video_post_args = reqparse.RequestParser()
+video_post_args.add_argument(
     "name", type=str, help="name of the video is required", required=True)
-video_put_args.add_argument(
+video_post_args.add_argument(
     "views", type=int, help="views of the video is required", required=True)
-video_put_args.add_argument(
+video_post_args.add_argument(
     "likes", type=int, help="likes on the video is required", required=True)
 
 
@@ -58,7 +58,7 @@ class Video(Resource):
 
     @marshal_with(resource_fields)
     def post(self, video_id):
-        args = video_put_args.parse_args()
+        args = video_post_args.parse_args()
         
         result = VideoModel.query.filter_by(id=video_id).first()
         if result:
